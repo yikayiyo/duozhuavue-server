aaca721114d0e1cbc87c839b8f58c4d6374e8c54
yikayiyo <837937787@qq.com>
2021-12-22 16:51:36 +0800

    add mutation to toggle book in the bookshelf list

--- src/graphql/book.js
+++ src/graphql/book.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const bookType = `
 	type Book {
 		id: ID!
@@ -26,6 +28,39 @@ const bookResolver = {
 		},
 		books: async (_, __, { models }) => {
 			return await models.Book.find({});
+		},
+	},
+	Mutation: {
+		toggleBookshelf: async (_, { bookId, userId }, { models }) => {
+			const user = await models.User.findById(userId);
+			const hasBook = user.bookShelf.indexOf(bookId);
+			// if the book doen't exist in the bookshelf list
+			if (hasBook < 0) {
+				return await models.User.findByIdAndUpdate(
+					userId,
+					{
+						$push: {
+							bookShelf: mongoose.Types.ObjectId(bookId),
+						},
+					},
+					// to return the updated doc
+					{
+						new: true,
+					}
+				);
+			} else {
+				return await models.User.findByIdAndUpdate(
+					userId,
+					{
+						$pull: {
+							bookShelf: mongoose.Types.ObjectId(bookId),
+						},
+					},
+					{
+						new: true,
+					}
+				);
+			}
 		},
 	},
 };
--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -3,6 +3,7 @@ const mutationType = `
     "注册、登录，成功时返回一个JWT，失败时？"
     signUp(name: String!, email: String!, password: String!): SignInOutResponse!
     signIn(email: String!, password: String!): SignInOutResponse!
+    toggleBookshelf(bookId: ID!, userId: ID!): User!
   }
 
   type SignInOutResponse {
