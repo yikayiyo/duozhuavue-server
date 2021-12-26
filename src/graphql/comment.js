8c00b63721b4018a21f5fb3fdd5089fe763f0098
yikayiyo <837937787@qq.com>
2021-12-26 22:39:33 +0800

    feat: add more comment resolvers

--- src/graphql/comment.js
+++ src/graphql/comment.js
@@ -15,6 +15,18 @@ const commentResolver = {
 			return await models.Comment.findById(id);
 		},
 	},
+	Comment: {
+		commenter: async ({ commenter }, _, { models }) => {
+			return await models.User.findById(commenter);
+		},
+		voters: async ({ voters }, _, { models }) => {
+			return await models.User.find({
+				_id: {
+					$in: voters,
+				},
+			});
+		},
+	},
 };
 
 module.exports = {
